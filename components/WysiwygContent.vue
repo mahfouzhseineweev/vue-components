7e477a125af8bf2ed92fc6cc96081e71ff615297
Hussein Mahfouz <h.mahfouz@eweev.com>
2025-04-03 13:46:04 +0300

    Wysiwig bullet list bug #91

--- components/WysiwygContent.vue
+++ components/WysiwygContent.vue
@@ -51,7 +51,7 @@ export default {
 .ql-editor ul>li:before {
   content: "\2022";
 }
-.ql-editor ol li:before {
+.ql-editor ol li:not([data-list="bullet"]):before {
   content: counter(list-0, decimal) ". ";
 }
 .ql-editor ol li {
